9f2416aaef8fc734537405065afa3c93cd435110
inga911 <inga.banaityte221@gmail.com>
2024-05-17 18:32:49 +0300

    main js logic

--- script.js
+++ script.js
@@ -9,11 +9,17 @@ const funAmountHTML = document.querySelector('.fun-text');
 const healthPointsProgressBar = document.querySelector('.health-points');
 const hungryLevelProgressBar = document.querySelector('.hungry-level');
 const funAmountProgressBar = document.querySelector('.fun-amount');
+const lifeTimer = document.querySelector('.life-timer');
+const feedBtn = document.querySelector('.feed');
+const playBtn = document.querySelector('.play');
 main.classList.add('d-none');
-// start.classList.add('d-none')
+yourAnimal.classList.add('animation-move');
 let hpLevel = 100;
 let hungryLevel = 100;
 let funLevel = 100;
+let timer = 60;
+let gameLevel = 1;
+let timerInterval;
 healthPointsHTML.innerHTML = `Hp ${hpLevel}%`;
 hungryLevelHTML.innerHTML = `Hungry ${hungryLevel}%`;
 funAmountHTML.innerHTML = `Fun ${funLevel}%`;
@@ -30,9 +36,15 @@ animalBoxes.forEach(a => {
         start.classList.add('d-none');
         main.classList.remove('d-none');
         yourAnimal.innerHTML = `${a.textContent}`;
+        yourAnimal.style.fontSize = '40px';
+        yourAnimal.style.top = '0px';
+        startGame();
     };
 });
-function updateProgressBar(hp, bar) {
+function startGame() {
+    timerInterval = setInterval(updateTimer, 1000);
+}
+function updateProgressBar(hp, bar, htmlEl) {
     bar.style.width = hp + '%';
     if (hp >= 75) {
         bar.style.backgroundColor = 'lightgreen';
@@ -46,7 +58,72 @@ function updateProgressBar(hp, bar) {
     else {
         bar.style.backgroundColor = 'white';
     }
+    updateBarsHtml(hp, htmlEl);
 }
-updateProgressBar(hpLevel, healthPointsProgressBar);
-updateProgressBar(hungryLevel, hungryLevelProgressBar);
-updateProgressBar(funLevel, funAmountProgressBar);
+function updateBarsHtml(level, htmlEl) {
+    htmlEl.innerHTML = `${htmlEl.classList.contains('health-text') ? 'Hp' : htmlEl.classList.contains('hungry-text') ? 'Hungry' : 'Fun'} ${level.toFixed(2)}%`;
+}
+updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML);
+updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
+function endGame(message) {
+    yourAnimal.classList.remove('animation-move');
+    clearInterval(timerInterval);
+    //@ts-ignore
+    feedBtn.disabled = true;
+    //@ts-ignore
+    playBtn.disabled = true;
+    alert(message);
+    start.classList.remove('d-none');
+    main.classList.add('d-none');
+    location.reload();
+}
+function updateTimer() {
+    hungryLevel -= 1.2;
+    updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML);
+    if (hungryLevel <= 0) {
+        hungryLevel = 0;
+        updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML);
+        endGame('Game Over: Your pet has starved to death.');
+        return;
+    }
+    funLevel -= 0.8;
+    updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
+    if (funLevel <= 0) {
+        funLevel = 0;
+        hpLevel -= 20;
+        updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
+        updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+    }
+    hpLevel -= 0.1;
+    updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+    lifeTimer.innerHTML = `Life timer: ${timer}s`;
+    timer--;
+    if (timer < 0) {
+        clearInterval(timerInterval);
+    }
+}
+feedBtn.onclick = () => {
+    if (hungryLevel < 100) {
+        hungryLevel += 1.2;
+        if (hungryLevel > 100) {
+            hungryLevel = 100;
+        }
+        updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML);
+    }
+    else {
+        console.log('i am full');
+    }
+};
+playBtn.onclick = () => {
+    if (funLevel < 100) {
+        funLevel += 1.2;
+        if (funLevel > 100) {
+            funLevel = 100;
+        }
+        updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
+    }
+    else {
+        console.log('i am happy!');
+    }
+};
--- script.ts
+++ script.ts
@@ -10,16 +10,24 @@ const healthPointsProgressBar = document.querySelector('.health-points') as HTML
 const hungryLevelProgressBar = document.querySelector('.hungry-level') as HTMLElement
 const funAmountProgressBar = document.querySelector('.fun-amount') as HTMLElement
 
+const lifeTimer = document.querySelector('.life-timer') as HTMLElement
+
+const feedBtn = document.querySelector('.feed') as HTMLElement
+const playBtn = document.querySelector('.play') as HTMLElement
 
 
 
 
 main.classList.add('d-none')
-// start.classList.add('d-none')
+yourAnimal.classList.add('animation-move')
 
 let hpLevel: number = 100
 let hungryLevel: number = 100
 let funLevel: number = 100
+let timer: number = 60
+let gameLevel: number = 1
+let timerInterval: number;
+
 
 healthPointsHTML.innerHTML = `Hp ${hpLevel}%`
 hungryLevelHTML.innerHTML = `Hungry ${hungryLevel}%`
@@ -40,11 +48,17 @@ animalBoxes.forEach(a => {
         start.classList.add('d-none')
         main.classList.remove('d-none')
         yourAnimal.innerHTML = `${a.textContent}`
-    }
-})
+        yourAnimal.style.fontSize = '40px'
+        yourAnimal.style.top = '0px'
+        startGame();
+    };
+});
 
+function startGame() {
+    timerInterval = setInterval(updateTimer, 1000);
+}
 
-function updateProgressBar(hp: number, bar: HTMLElement) {
+function updateProgressBar(hp: number, bar: HTMLElement, htmlEl: HTMLElement) {
     bar.style.width = hp + '%';
     if (hp >= 75) {
         bar.style.backgroundColor = 'lightgreen'
@@ -55,8 +69,80 @@ function updateProgressBar(hp: number, bar: HTMLElement) {
     } else {
         bar.style.backgroundColor = 'white'
     }
+    updateBarsHtml(hp, htmlEl);
+
 }
 
-updateProgressBar(hpLevel, healthPointsProgressBar)
-updateProgressBar(hungryLevel, hungryLevelProgressBar)
-updateProgressBar(funLevel, funAmountProgressBar)
+function updateBarsHtml(level: number, htmlEl: HTMLElement) {
+    htmlEl.innerHTML = `${htmlEl.classList.contains('health-text') ? 'Hp' : htmlEl.classList.contains('hungry-text') ? 'Hungry' : 'Fun'} ${level.toFixed(2)}%`;
+}
+
+updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML)
+updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML)
+updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
+
+
+function endGame(message: string) {
+    yourAnimal.classList.remove('animation-move')
+    clearInterval(timerInterval);
+    //@ts-ignore
+    feedBtn.disabled = true;
+    //@ts-ignore
+    playBtn.disabled = true;
+    alert(message);
+    start.classList.remove('d-none')
+    main.classList.add('d-none')
+    location.reload();
+}
+
+function updateTimer() {
+    hungryLevel -= 1.2
+    updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML)
+    if (hungryLevel <= 0) {
+        hungryLevel = 0
+        updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML);
+        endGame('Game Over: Your pet has starved to death.');
+        return;
+    }
+    funLevel -= 0.8
+    updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
+    if (funLevel <= 0) {
+        funLevel = 0
+        hpLevel -= 20
+        updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
+        updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+    }
+    hpLevel -= 0.1
+    updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML)
+    lifeTimer.innerHTML = `Life timer: ${timer}s`
+    timer--
+    if (timer < 0) {
+        clearInterval(timerInterval)
+    }
+}
+
+
+feedBtn.onclick = () => {
+    if (hungryLevel < 100) {
+        hungryLevel += 1.2
+        if (hungryLevel > 100) {
+            hungryLevel = 100
+        }
+        updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML)
+    } else {
+        console.log('i am full');
+    }
+}
+
+
+playBtn.onclick = () => {
+    if (funLevel < 100) {
+        funLevel += 1.2
+        if (funLevel > 100) {
+            funLevel = 100
+        }
+        updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
+    } else {
+        console.log('i am happy!');
+    }
+}
