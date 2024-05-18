1bb9b5c707b0b68d95c8925450f0bfcaedc983a0
inga911 <inga.banaityte221@gmail.com>
2024-05-18 20:18:20 +0300

    night function

--- script.js
+++ script.js
@@ -3,26 +3,37 @@ const start = document.querySelector('.start');
 const selectAnimalContainer = document.querySelector('.select-animal');
 const yourAnimal = document.querySelector('.your-animal');
 const main = document.querySelector('.main');
+const mainGame = document.querySelector('.main-game');
+const gameLevelBox = document.querySelector('.level-box');
 const healthPointsHTML = document.querySelector('.health-text');
 const hungryLevelHTML = document.querySelector('.hungry-text');
 const funAmountHTML = document.querySelector('.fun-text');
+const sleepLevelHTML = document.querySelector('.sleep-text');
 const healthPointsProgressBar = document.querySelector('.health-points');
 const hungryLevelProgressBar = document.querySelector('.hungry-level');
 const funAmountProgressBar = document.querySelector('.fun-amount');
+const sleepLevelProgressBar = document.querySelector('.sleep-level');
 const lifeTimer = document.querySelector('.life-timer');
 const feedBtn = document.querySelector('.feed');
 const playBtn = document.querySelector('.play');
+const sleepBtn = document.querySelector('.sleep');
 main.classList.add('d-none');
 yourAnimal.classList.add('animation-move');
 let hpLevel = 100;
 let hungryLevel = 100;
 let funLevel = 100;
+let sleepLevel = 100;
 let timer = 60;
 let gameLevel = 1;
 let timerInterval;
+let hasDecreasedHP = false;
+let yourAnimalSize = 40;
+let yourAnimalTopPosition = 0;
 healthPointsHTML.innerHTML = `Hp ${hpLevel}%`;
 hungryLevelHTML.innerHTML = `Hungry ${hungryLevel}%`;
 funAmountHTML.innerHTML = `Fun ${funLevel}%`;
+sleepLevelHTML.innerHTML = `Sleep ${sleepLevel}%`;
+gameLevelBox.innerHTML = `Level: ${gameLevel}`;
 let animal = ['🐶', '🐱', '🦊', '🐼', '🐯', '🐮', '🦥', '🐇', '🦜', '🐠', '🐿', '🐉', '🦒', '🦓', '🦄', '🐧'];
 for (let i = 0; i < animal.length; i++) {
     selectAnimalContainer.innerHTML += `
@@ -36,8 +47,8 @@ animalBoxes.forEach(a => {
         start.classList.add('d-none');
         main.classList.remove('d-none');
         yourAnimal.innerHTML = `${a.textContent}`;
-        yourAnimal.style.fontSize = '40px';
-        yourAnimal.style.top = '0px';
+        yourAnimal.style.fontSize = `${yourAnimalSize}px`;
+        yourAnimal.style.top = `${yourAnimalTopPosition}px`;
         startGame();
     };
 });
@@ -66,6 +77,7 @@ function updateBarsHtml(level, htmlEl) {
 updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
 updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML);
 updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
+updateProgressBar(sleepLevel, sleepLevelProgressBar, sleepLevelHTML);
 function endGame(message) {
     yourAnimal.classList.remove('animation-move');
     clearInterval(timerInterval);
@@ -78,8 +90,8 @@ function endGame(message) {
     main.classList.add('d-none');
     location.reload();
 }
-function updateTimer() {
-    hungryLevel -= 1.2;
+function hungry() {
+    hungryLevel -= 0.8;
     updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML);
     if (hungryLevel <= 0) {
         hungryLevel = 0;
@@ -87,16 +99,49 @@ function updateTimer() {
         endGame('Game Over: Your pet has starved to death.');
         return;
     }
-    funLevel -= 0.8;
+}
+function sleep() {
+    sleepLevel -= 1;
+    updateProgressBar(sleepLevel, sleepLevelProgressBar, sleepLevelHTML);
+    if (sleepLevel === 0) {
+        sleepLevel = 0;
+        hpLevel = 0;
+        updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+        endGame('Game Over: Your pet has died because was too tired.');
+        return;
+    }
+}
+function fun() {
+    funLevel -= 1.3;
     updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
     if (funLevel <= 0) {
         funLevel = 0;
-        hpLevel -= 20;
+        if (!hasDecreasedHP) {
+            hpLevel -= 20;
+            hasDecreasedHP = true;
+        }
         updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
         updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
     }
-    hpLevel -= 0.1;
+    else {
+        hasDecreasedHP = false;
+    }
+}
+function healthPoints(hungry, fun, sleep) {
+    hpLevel -= 0.5;
     updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+    if (hungry === 0 && fun === 0 && sleep === 0) {
+        hpLevel = 0;
+        updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+        endGame('Game Over: Your pet died..');
+        return;
+    }
+}
+function updateTimer() {
+    hungry();
+    sleep();
+    fun();
+    healthPoints(hungryLevel, funLevel, sleepLevel);
     lifeTimer.innerHTML = `Life timer: ${timer}s`;
     timer--;
     if (timer < 0) {
@@ -104,8 +149,8 @@ function updateTimer() {
     }
 }
 feedBtn.onclick = () => {
-    if (hungryLevel < 100) {
-        hungryLevel += 1.2;
+    if (hungryLevel < 70) {
+        hungryLevel += 0.8;
         if (hungryLevel > 100) {
             hungryLevel = 100;
         }
@@ -116,8 +161,8 @@ feedBtn.onclick = () => {
     }
 };
 playBtn.onclick = () => {
-    if (funLevel < 100) {
-        funLevel += 1.2;
+    if (funLevel < 80) {
+        funLevel += 1.3;
         if (funLevel > 100) {
             funLevel = 100;
         }
@@ -125,5 +170,44 @@ playBtn.onclick = () => {
     }
     else {
         console.log('i am happy!');
+    }
+};
+function nightStart() {
+    yourAnimal.classList.add('animation-move');
+    mainGame.classList.remove('nightbg');
+    playBtn.classList.remove('button-nigth');
+    feedBtn.classList.remove('button-nigth');
+    sleepBtn.classList.remove('button-nigth');
+    mainGame.style.color = 'black';
+}
+function nightEnd() {
+    yourAnimal.classList.remove('animation-move');
+    mainGame.classList.add('nightbg');
+    mainGame.style.color = 'yellow';
+    playBtn.classList.add('button-nigth');
+    feedBtn.classList.add('button-nigth');
+    sleepBtn.classList.add('button-nigth');
+}
+sleepBtn.onclick = () => {
+    if (sleepLevel < 95) {
+        nightEnd();
+        clearInterval(timerInterval);
+        setTimeout(() => {
+            startGame();
+            nightStart();
+            if (sleepLevel === 100) {
+                gameLevel++;
+                gameLevelBox.innerHTML = `Level: ${gameLevel}`;
+                yourAnimalSize += 10;
+                yourAnimal.style.fontSize = `${yourAnimalSize}px`;
+                yourAnimalTopPosition -= 15;
+                yourAnimal.style.top = `${yourAnimalTopPosition}px`;
+            }
+        }, 15000);
+        sleepLevel = 100;
+        updateProgressBar(sleepLevel, sleepLevelProgressBar, sleepLevelHTML);
+    }
+    else {
+        console.log('i am rest!');
     }
 };
--- script.ts
+++ script.ts
@@ -2,21 +2,24 @@ const start = document.querySelector('.start') as HTMLElement
 const selectAnimalContainer = document.querySelector('.select-animal') as HTMLElement
 const yourAnimal = document.querySelector('.your-animal') as HTMLElement
 const main = document.querySelector('.main') as HTMLElement
+const mainGame = document.querySelector('.main-game') as HTMLElement
+const gameLevelBox = document.querySelector('.level-box') as HTMLElement
 
 const healthPointsHTML = document.querySelector('.health-text') as HTMLElement
 const hungryLevelHTML = document.querySelector('.hungry-text') as HTMLElement
 const funAmountHTML = document.querySelector('.fun-text') as HTMLElement
+const sleepLevelHTML = document.querySelector('.sleep-text') as HTMLElement
+
 const healthPointsProgressBar = document.querySelector('.health-points') as HTMLElement
 const hungryLevelProgressBar = document.querySelector('.hungry-level') as HTMLElement
 const funAmountProgressBar = document.querySelector('.fun-amount') as HTMLElement
+const sleepLevelProgressBar = document.querySelector('.sleep-level') as HTMLElement
 
 const lifeTimer = document.querySelector('.life-timer') as HTMLElement
 
 const feedBtn = document.querySelector('.feed') as HTMLElement
 const playBtn = document.querySelector('.play') as HTMLElement
-
-
-
+const sleepBtn = document.querySelector('.sleep') as HTMLElement
 
 main.classList.add('d-none')
 yourAnimal.classList.add('animation-move')
@@ -24,14 +27,19 @@ yourAnimal.classList.add('animation-move')
 let hpLevel: number = 100
 let hungryLevel: number = 100
 let funLevel: number = 100
+let sleepLevel: number = 100
 let timer: number = 60
 let gameLevel: number = 1
 let timerInterval: number;
+let hasDecreasedHP: boolean = false;
+let yourAnimalSize: number = 40
+let yourAnimalTopPosition: number = 0
 
-
-healthPointsHTML.innerHTML = `Hp ${hpLevel}%`
-hungryLevelHTML.innerHTML = `Hungry ${hungryLevel}%`
-funAmountHTML.innerHTML = `Fun ${funLevel}%`
+healthPointsHTML.innerHTML = `Hp ${hpLevel}%`;
+hungryLevelHTML.innerHTML = `Hungry ${hungryLevel}%`;
+funAmountHTML.innerHTML = `Fun ${funLevel}%`;
+sleepLevelHTML.innerHTML = `Sleep ${sleepLevel}%`;
+gameLevelBox.innerHTML = `Level: ${gameLevel}`;
 
 let animal: string[] = ['🐶', '🐱', '🦊', '🐼', '🐯', '🐮', '🦥', '🐇', '🦜', '🐠', '🐿', '🐉', '🦒', '🦓', '🦄', '🐧']
 
@@ -48,8 +56,8 @@ animalBoxes.forEach(a => {
         start.classList.add('d-none')
         main.classList.remove('d-none')
         yourAnimal.innerHTML = `${a.textContent}`
-        yourAnimal.style.fontSize = '40px'
-        yourAnimal.style.top = '0px'
+        yourAnimal.style.fontSize = `${yourAnimalSize}px`
+        yourAnimal.style.top = `${yourAnimalTopPosition}px`
         startGame();
     };
 });
@@ -80,6 +88,7 @@ function updateBarsHtml(level: number, htmlEl: HTMLElement) {
 updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML)
 updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML)
 updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
+updateProgressBar(sleepLevel, sleepLevelProgressBar, sleepLevelHTML)
 
 
 function endGame(message: string) {
@@ -95,8 +104,8 @@ function endGame(message: string) {
     location.reload();
 }
 
-function updateTimer() {
-    hungryLevel -= 1.2
+function hungry() {
+    hungryLevel -= 0.8
     updateProgressBar(hungryLevel, hungryLevelProgressBar, hungryLevelHTML)
     if (hungryLevel <= 0) {
         hungryLevel = 0
@@ -104,16 +113,52 @@ function updateTimer() {
         endGame('Game Over: Your pet has starved to death.');
         return;
     }
-    funLevel -= 0.8
-    updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
-    if (funLevel <= 0) {
-        funLevel = 0
-        hpLevel -= 20
-        updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
+}
+
+function sleep() {
+    sleepLevel -= 1
+    updateProgressBar(sleepLevel, sleepLevelProgressBar, sleepLevelHTML)
+    if (sleepLevel === 0) {
+        sleepLevel = 0
+        hpLevel = 0
         updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+        endGame('Game Over: Your pet has died because was too tired.');
+        return;
     }
-    hpLevel -= 0.1
+}
+
+function fun() {
+    funLevel -= 1.3;
+    updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
+    if (funLevel <= 0) {
+        funLevel = 0;
+        if (!hasDecreasedHP) {
+            hpLevel -= 20;
+            hasDecreasedHP = true;
+        }
+        updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML);
+        updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML);
+    } else {
+        hasDecreasedHP = false;
+    }
+}
+
+
+function healthPoints(hungry: number, fun: number, sleep: number) {
+    hpLevel -= 0.5
     updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML)
+    if (hungry === 0 && fun === 0 && sleep === 0) {
+        hpLevel = 0
+        updateProgressBar(hpLevel, healthPointsProgressBar, healthPointsHTML)
+        endGame('Game Over: Your pet died..');
+        return;
+    }
+}
+function updateTimer() {
+    hungry()
+    sleep()
+    fun()
+    healthPoints(hungryLevel, funLevel, sleepLevel)
     lifeTimer.innerHTML = `Life timer: ${timer}s`
     timer--
     if (timer < 0) {
@@ -123,8 +168,8 @@ function updateTimer() {
 
 
 feedBtn.onclick = () => {
-    if (hungryLevel < 100) {
-        hungryLevel += 1.2
+    if (hungryLevel < 70) {
+        hungryLevel += 0.8
         if (hungryLevel > 100) {
             hungryLevel = 100
         }
@@ -136,13 +181,55 @@ feedBtn.onclick = () => {
 
 
 playBtn.onclick = () => {
-    if (funLevel < 100) {
-        funLevel += 1.2
+    if (funLevel < 80) {
+        funLevel += 1.3
         if (funLevel > 100) {
             funLevel = 100
         }
         updateProgressBar(funLevel, funAmountProgressBar, funAmountHTML)
     } else {
         console.log('i am happy!');
+    }
+}
+
+function nightStart() {
+    yourAnimal.classList.add('animation-move')
+    mainGame.classList.remove('nightbg')
+    playBtn.classList.remove('button-nigth')
+    feedBtn.classList.remove('button-nigth')
+    sleepBtn.classList.remove('button-nigth')
+    mainGame.style.color = 'black'
+}
+
+function nightEnd() {
+    yourAnimal.classList.remove('animation-move')
+    mainGame.classList.add('nightbg')
+    mainGame.style.color = 'yellow'
+    playBtn.classList.add('button-nigth')
+    feedBtn.classList.add('button-nigth')
+    sleepBtn.classList.add('button-nigth')
+}
+
+sleepBtn.onclick = () => {
+    if (sleepLevel < 95) {
+        nightEnd()
+        clearInterval(timerInterval);
+        setTimeout(() => {
+            startGame();
+            nightStart()
+
+            if (sleepLevel === 100) {
+                gameLevel++
+                gameLevelBox.innerHTML = `Level: ${gameLevel}`
+                yourAnimalSize += 10
+                yourAnimal.style.fontSize = `${yourAnimalSize}px`
+                yourAnimalTopPosition -= 15
+                yourAnimal.style.top = `${yourAnimalTopPosition}px`
+            }
+        }, 15000);
+        sleepLevel = 100
+        updateProgressBar(sleepLevel, sleepLevelProgressBar, sleepLevelHTML)
+    } else {
+        console.log('i am rest!');
     }
 }
